c1b7bf78379c5dbb898d0d36e6554b3408de222a
Chee Ng <cwng@ncsu.edu>
2020-07-21 02:19:19 -0400

    All ref are working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import styles from './App.module.css';
+
+import {Cards, Chart, CountryPicker } from './components';
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Cards></Cards>
+        <CountryPicker></CountryPicker>
+        <Chart></Chart>
         <h1>App</h1>
       </div>
     )
