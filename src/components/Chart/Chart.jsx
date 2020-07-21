d2e8e6a11fa98a958814ca2146a2b91a6b895efe
Chee Ng <cwng@ncsu.edu>
2020-07-21 05:53:38 -0400

    Implemented chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { fetchDailyData } from '../../api';
+import {Line, Bar } from 'react-chartjs-2';
+import styles from './Chart.module.css';
 
 const Chart = () => {
-    return(<h1>Charts</h1>)
+    // similar way of state: {data}
+    const [dailyData, setDailyData] = useState([]);
+
+    useEffect(()=>{
+        const fetchApi = async () => {
+            //const dailyData = await fetchDailyData();
+            setDailyData(await fetchDailyData())
+        }
+
+        //console.log(dailyData);
+
+        fetchApi();
+    });
+    //if there is no daily data first day return null
+    const lineChart = (
+        dailyData.length ? (
+        // daily data only available for infected and death
+        <Line data ={{
+            labels: dailyData.map(({date}) => date),
+            datasets: [{
+                data: dailyData.map(({confirmed})=>confirmed),
+                label: 'Infected', 
+                borderColor: '#3333ff', 
+                fill: true },
+            {
+                data: dailyData.map(({deaths})=>deaths),
+                label: 'Deaths', 
+                borderColor: 'red',
+                backgroundColor: 'rgba(255, 0, 0, 0.5)', 
+                fill: true 
+            }],
+        }}
+        />) : null
+    
+    );
+
+
+    return(<div className={styles.container}>{lineChart}</div>)
 }
 
 export default Chart;
