4e62872a7faa7da7eb9cf0480eb6eb9ac5192ba7
Chee Ng <cwng@ncsu.edu>
2020-07-21 02:44:30 -0400

    Implemented Async/Await and tested api output

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import styles from './App.module.css';
+import styles from './App.module.css'; // styles.container to make sure there is no interferace from other css file
 
+//Unique and cleanier way to import
 import {Cards, Chart, CountryPicker } from './components';
+// Similar to this
 // import Cards from './components/Cards/Cards';
 // import Chart from './components/Chart/Chart';
 // import CountryPicker from './components/CountryPicker/CountryPicker';
 
+import{ fetchData } from './api';
+
 class App extends React.Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
   render() {
     return (
-      <div>
+      <div className={styles.container} >
         <Cards></Cards>
         <CountryPicker></CountryPicker>
         <Chart></Chart>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+ // For app.js import
  export { default as Cards } from './Cards/Cards';
  export { default as Chart } from './Chart/Chart';
  export { default as CountryPicker } from './CountryPicker/CountryPicker';
